a20542ae77bf18c6dfd66c0e7bd325b44f760b92
Mehman Aliyev <mehman.va@gmail.com>
2023-09-15 21:45:57 +0400

    add keypress event in input

--- list.js
+++ list.js
@@ -6,6 +6,30 @@ const btnDanger = document.getElementById("btnDanger");
 
 let count = 0;
 
+
+inp.addEventListener("keypress", event=>{
+    if(event.key === "Enter"){
+        let text = inp.value.trim();
+
+        if(text != ""){
+            ul.innerHTML += `<li class="list-group-item w-100 mt-2"> ${text} </li>`; 
+            count++;
+            inp.value = "";
+            console.log(count);
+            if(count == 1){
+                btnDanger.classList.remove("d-none");
+                btnDanger.innerHTML = "Remove user"
+            }else{
+                btnDanger.classList.remove("d-none");
+                btnDanger.innerHTML = "Remove all user"
+            }
+        }else {
+            alert("Xana doldurulmayib");
+        } 
+    }
+})
+
+
 btn.addEventListener("click", ()=>{
     
     let text = inp.value.trim();
