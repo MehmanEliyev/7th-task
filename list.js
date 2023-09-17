46d777aed924b4f3d71bf2390635fbbf61104310
Mehman Aliyev <mehman.va@gmail.com>
2023-09-17 12:51:48 +0400

    complete toDo task with number

--- list.js
+++ list.js
@@ -2,65 +2,111 @@ const inp = document.getElementById("custom-input");
 const btn = document.getElementById("custom-button");
 const ul = document.querySelector(".unorderList");
 const btnDanger = document.getElementById("btnDanger");
+const numberList = document.querySelector(".numberList")
 
 let count = 0;
+let quantity = 0;
+let arr = [];
 
-    //! ------------ Enter click  -----------------------------
+//! ------------------ Enter click  -----------------------------
 
 inp.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
+
     let text = inp.value.trim();
+  
+  if (text != "") {
 
-    if (text != "") {
-        ul.innerHTML += `<li class="list-group-item listItem mt-2 position-relative"> ${text} <button  class="btn text-danger deleteBtn "> X </button> </li> `;
-        const listItem = document.getElementById(`${count}`);
-        inp.value = "";
-        count++;
+    quantity++;
+    arr.push(quantity);
+
+    ul.innerHTML += `
+    <li class="list-group-item listItem mt-2 ml-4 position-relative"> ${text} <span class="quantity"></span>
+    <button class="btn text-danger deleteBtn "> X </button> </li> `;
+
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.classList.add("mt-2");
     
-        const deleteBtn = document.querySelectorAll(".deleteBtn");
+    arr.forEach(element => {
+      li.innerText = element;
+      numberList.appendChild(li);
+    });
 
-        if (count > 1) {
-          btnDanger.classList.remove("d-none");
-          btnDanger.innerHTML = "Remove all user";
-        }else if(count == 1){
+    inp.value = "";
+    count++;
+    
+    if (count > 1) {
+      btnDanger.classList.remove("d-none");
+      btnDanger.innerHTML = "Remove all user";
+    }else if(count == 1){
+      btnDanger.classList.remove("d-none");
+      btnDanger.innerHTML = "Remove user";
+    }
+    
+    const deleteBtn = document.querySelectorAll(".deleteBtn");
+    
+    deleteBtn.forEach((element) => {
+      element.addEventListener("click", () => {
+        
+        element.parentElement.remove();
+
+        numberList.innerHTML = "";
+        arr.pop();
+        
+        arr.forEach(element => {
+          const li = document.createElement("li");
+          li.classList.add("list-group-item");
+          li.classList.add("mt-2");
+          li.innerText += element;
+          numberList.appendChild(li);
+        });
+        
+        quantity = arr.length;
+        count--;
+        
+        if(count == 0){
+          btnDanger.classList.add("d-none");
+        } else if(count == 1){
           btnDanger.classList.remove("d-none");
           btnDanger.innerHTML = "Remove user";
         }
+      });
+    });
     
-        deleteBtn.forEach((element) => {
-          element.addEventListener("click", () => {
-            element.parentElement.remove();
-            count--;
-            if(count == 0){
-              btnDanger.classList.add("d-none");
-            } else if(count == 1){
-              btnDanger.classList.remove("d-none");
-              btnDanger.innerHTML = "Remove user";
-            }
-          });
-        });
-
-
-    } else {
-      alert("Xana doldurulmayib");
-    }
+  } else {
+    alert("Xana doldurulmayib");
+  }
   }
 });
 
-    //! ------------ button click  -----------------------------
+//! -------------------- Button click  -----------------------------
 
 btn.addEventListener("click", () => {
-  let text = inp.value.trim();
-
+  
+  let text = inp.value.trim().slice(0,115);
+  
   if (text != "") {
-    ul.innerHTML += `<li id="${count}li" class="list-group-item listItem mt-2 position-relative"> ${text} <button  class="btn text-danger deleteBtn "> X </button> </li> `;
-    const listItem = document.getElementById(`${count}`);
+
+    quantity++;
+    arr.push(quantity);
+
+    ul.innerHTML += `
+    <li class="list-group-item listItem mt-2 ml-4 position-relative"> ${text} <span class="quantity"></span>
+    <button class="btn text-danger deleteBtn "> X </button> </li> `;
+
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.classList.add("mt-2");
+    
+    arr.forEach(element => {
+      li.innerText = element;
+      numberList.appendChild(li);
+    });
+
     inp.value = "";
     count++;
-    console.log("add " + count);
-
-    const deleteBtn = document.querySelectorAll(".deleteBtn");
-
+    
     if (count > 1) {
       btnDanger.classList.remove("d-none");
       btnDanger.innerHTML = "Remove all user";
@@ -69,11 +115,27 @@ btn.addEventListener("click", () => {
       btnDanger.innerHTML = "Remove user";
     }
 
+    const deleteBtn = document.querySelectorAll(".deleteBtn");
+    
     deleteBtn.forEach((element) => {
       element.addEventListener("click", () => {
+        
         element.parentElement.remove();
+
+        numberList.innerHTML = "";
+        arr.pop();
+        
+        arr.forEach(element => {
+          const li = document.createElement("li");
+          li.classList.add("list-group-item");
+          li.classList.add("mt-2");
+          li.innerText += element;
+          numberList.appendChild(li);
+        });
+        
+        quantity = arr.length;
         count--;
-        console.log("del " + count)
+        
         if(count == 0){
           btnDanger.classList.add("d-none");
         } else if(count == 1){
@@ -88,8 +150,13 @@ btn.addEventListener("click", () => {
   }
 });
 
+//! -------------------- Danger button click  -----------------------------
+
 btnDanger.addEventListener("click", () => {
   ul.innerHTML = "";
+  numberList.innerHTML = "";
+  arr = [];
+  quantity = arr.length;
   btnDanger.classList.add("d-none");
   count = 0;
 });
